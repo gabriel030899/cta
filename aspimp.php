a23806eefbfc9af34a8820132dfcbb46567e1064
Silva <Gabriel.Silva14@br.nestle.com>
2021-09-13 10:32:53 -0300

    Cards SHE, Video e Botao questinario estilizados

--- aspimp.php
+++ aspimp.php
@@ -14,35 +14,34 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
-
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
-
-
     <body onlo id="page-top">
-
-
-
-        <!-- Barra de Navegação  -->
-        
-        
-        <!-- Masthead-->
-
         <!-- About-->
         <section class="about-section text-center" id="about">
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-lg-8">
-                        <h2 class="text-white mb-4">ASMPIMP E LPD</h2>
-                                  
-        
+                    <div class="col-lg-8" id="titlediv">
+                        <h1 class="text-white mb-4">ASMPIMP E LPD</h1>
                     </div>
                 </div>
-
             <style>
-
-               
-            
+            #about{
+                padding: 0;
+                height: 85vh;
+            }
+            #titlediv{
+                height: 120px;
+                margin: 0 0 20px 0;
+                width: auto;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                border: 2px solid white;
+                border-top: 0;
+                border-right: 0;
+                border-left: 0;
+            }
             .pilaresformat{
                 display: flex;
                 justify-content:center;
@@ -51,87 +50,60 @@
                 height: 200px;
                 padding:10px;
                 margin-bottom: 10px;
-
             }
-            
-            
             #mae-pilares {
-                
                 display:flex;
                 width:100%;
                 justify-content:center;
                 align-items:center;
                 margin-bottom: 60px;
-                
             }
             #carouselExampleControls{
                 margin-top:30px;
             }
-
             #video {
-
                 align-items: center;
                 justify-content: center;
-
             }
-
+            #video video{
+                width: 1100px;
+            }
             #questionarioButton {
-
                 align-items: center;
                 justify-content: center;
             }
-            
-            
+            #projects{
+                padding: 0;
+                height: 15vh;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            #btnSpeak{
+                height: 70px;
+            }
             </style>
-
-
                 <div id="mae-pilares">
-
-
-                <div id="video">
-
-
-                <video width="1200" height="600" controls>
-                <source src="assets/movies/aspimp.mp4" type="video/mp4">
-                Your browser does not support the video tag.
-                </video>
-
-
-                </div>
-
-
-                
-                
-                
-                 </div>
-                
-                
+                    <div id="video">
+                        <video controls>
+                            <source src="assets/movies/aspimp.mp4" type="video/mp4">
+                                Your browser does not support the video tag.
+                        </video>
+                    </div>                
+                 </div>                
             </div>
         </section>
         <!-- Projects-->
-
         <section class="projects-section bg-light" id="projects">
             <div class="container px-4 px-lg-5">
                 <!-- Featured Project Row-->
-                
                 <center>
-                <div id="questionarioButton">
-                <a href="#about">  <button id="btnSpeak" class="btn btn-primary" > <img src="assets/img/question.png" width="20px" height="20px">  Responder Questionário </button> </a> 
-                </div>  
+                    <div id="questionarioButton">
+                        <a href="#about">  <button id="btnSpeak" class="btn btn-primary" > <img src="assets/img/question.png" width="30px" height="30px">  Responder Questionário </button> </a> 
+                    </div>  
                 </center>
-              
-
                 </div>
-                
-              
-
-
-       
         </section>
-        <!-- Signup-->
-       
-        <!-- Contact-->
-
         <!-- Footer-->
         <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy;  2021</div></footer>
         <!-- Bootstrap core JS-->
--- she.php
+++ she.php
@@ -17,312 +17,177 @@
 
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
-
-
-    <body onlo id="page-top">
-
-
-
-        <!-- Barra de Navegação  -->
-        
-        
-        <!-- Masthead-->
-
+    <body id="page-top">
         <!-- About-->
         <section class="about-section text-center" id="about">
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8">
                         <h2 class="text-white mb-4">SHE - Segurança, Saúde e Meio Ambiente</h2>
-                        
                         <div id="buttonSpeak">
-
                         <a href="#about">  <button id="btnSpeak" class="btn btn-primary" > 
-                        
                         <img src="assets/img/voice.png"> </button> </a> 
-                        
                         </div>
-
                         <p class="text-white-50">
                            As Ferramentas que compõem o pilar de SHE são fundamentais e de grande importância para o ambiente de trabalho, selecione um dos itens que deseja aprender! 
                         </p>
                     </div>
                 </div>
 
-            <style>
-
-                #buttonSpeak {
-
-                    margin-bottom: 10px;
-
-                    
+                <style>
+                #about{
+                    height: 100vh;
                 }
-            
-            #pilar1:hover {
-                
-                
-                background: url("assets/img/aspimp_hover.png") no-repeat;
-                background-position:center;
-                background-size:contain;
-
-
-                       }
-            
-            #pilar1 {
-                
-                
-            background: url("assets/img/lpd.png") no-repeat;
-            background-position:center;
-            background-size:contain;
-
-
-
-            }
-            
-            
-            
-              #pilar2 {
-                
-                
-                background: url("assets/img/BBS.png") no-repeat;
-                                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-                        #pilar2:hover {
-                
-                
-                background: url("assets/img/BBS_HOVER.png") no-repeat;
-                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-            
-                        
-              #pilar3 {
-                
-                
-                background: url("assets/img/mp.png") no-repeat;
-                                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-                        #pilar3:hover {
-                
-                
-                background: url("assets/img/mp_hover.png") no-repeat;
-                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-                                    
-              #pilar4 {
-                
-                
-                background: url("assets/img/she.png") no-repeat;
-                                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-                        #pilar4:hover {
-                
-                
-                background: url("assets/img/she_hover.png") no-repeat;
-                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-                                                
-              #pilar5 {
-                
-                
-                background: url("assets/img/qualidade.png") no-repeat;
-                                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-                        #pilar5:hover {
-                
-                
-                background: url("assets/img/qualidade_hover.png") no-repeat;
-                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-                                                
-              #pilar6 {
-                
-                
-                background: url("assets/img/mega.png") no-repeat;
-                                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-                        #pilar6:hover {
-                
-                
-                background: url("assets/img/mega_hover.png") no-repeat;
-                                                background-position:center;
-                                background-size:contain;
-
-
-            }
-            
-            
-            
-            
-            .pilaresformat{
-                display: flex;
-                justify-content:center;
-                align-items:center;
-                width: 700px;
-                height: 200px;
-                padding:10px;
-                margin-bottom: 10px;
-
-            }
-            
-            
-            #mae-pilares {
-                
-                display:flex;
-                width:100%;
-                justify-content:center;
-                align-items:center;
-                
-            }
-            #carouselExampleControls{
-                margin-top:30px;
-            }
-            
-            
-            </style>
-
+                #buttonSpeak {
+                    margin-bottom: 10px;
+                }
+                #pilar1:hover {
+                    background: url("assets/img/aspimp_hover.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }
+                #pilar1 {
+                    background: url("assets/img/lpd.png") no-repeat;
+                    background-position:center;
+                    background-size: contain;
+                    width: 600px;
+                    height: 200px;
+                    transform:rotateY(180deg) ;
+                }
+                #pilar1 a{
+                    width: 100%;
+                    height: 100%;
+                }
+                #pilar2 {
+                    background: url("assets/img/BBS.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                    width: 580px;
+                    height: 180px;
+                }
+                #pilar2 a{
+                    width: 100%;
+                    height: 100%;
+                }
+                #pilar2:hover {
+                    background: url("assets/img/BBS_HOVER.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }           
+                #pilar3 {   
+                    background: url("assets/img/mp.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }
+                #pilar3:hover {    
+                    background: url("assets/img/mp_hover.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }
+                #pilar4 {
+                    background: url("assets/img/she.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }
+                #pilar4:hover {    
+                    background: url("assets/img/she_hover.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }                           
+                #pilar5 {
+                    background: url("assets/img/qualidade.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }
+                #pilar5:hover {
+                    background: url("assets/img/qualidade_hover.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }
+                #pilar6 {
+                    background: url("assets/img/mega.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }
+                #pilar6:hover {
+                    background: url("assets/img/mega_hover.png") no-repeat;
+                    background-position:center;
+                    background-size:contain;
+                }
+                .pilaresformat{
+                    display: flex;
+                    justify-content:center;
+                    align-items:center;
+                    width: 700px;
+                    height: 200px;
+                    padding:10px;
+                    margin-bottom: 10px;
+                }
+                #mae-pilares {
+                    display:flex;
+                    width:100%;
+                    justify-content:center;
+                    align-items:center;
+                }
+                #carouselExampleControls{
+                    margin-top:30px;
+                }
+                </style>
 
                 <div id="mae-pilares">
-
-
-              <a href="direciona.php?codigo=s1"> <div id="pilar1" class="pilaresformat">  </div> </a> 
-              <a href=""> <div id="pilar2" class="pilaresformat">  </div> </a> 
-
-
-                
-                
-                
+                    <div id="pilar1" class="pilaresformat"><a href="direciona.php?codigo=s1"></a></div>
+                    <div id="pilar2" class="pilaresformat"><a href=""></a></div> 
                  </div>
-                
-                
             </div>
         </section>
-        <!-- Projects-->
-        
-        
-        
-        
-        
-        
         <section class="projects-section bg-light" id="projects">
             <div class="container px-4 px-lg-5">
                 <!-- Featured Project Row-->
                 <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-
                     <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>Pilar MA</h4>
                             <p class="text-black-50 mb-0">Breve descrição do pilar MA</p>
                             
                         </div>
-
                     </div>
-                    
-                                <div class="col-xl-4 col-lg-5">
+                    <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>Pilar MP</h4>
                             <p class="text-black-50 mb-0">Breve descrição do pilar MP</p>
                             
                         </div>
-
                     </div>
-                    
                       <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>SHE</h4>
                             <p class="text-black-50 mb-0">Breve descrição do pilar SHE</p>
                             
                         </div>
-
                     </div>
-                    
-           
-
                 </div>
-                
                  <div class="row gx-0 mb-4 mb-lg-5 align-items-center"> 
-                 
-                                      <div class="col-xl-4 col-lg-5">
+                    <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>Pilar Qualidade</h4>
                             <p class="text-black-50 mb-0">Breve descrição do pilar Qualidade</p>
-                            
                         </div>
-
                     </div>
-                    
-                                                       <div class="col-xl-4 col-lg-5">
+                    <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>Pilar MEGA</h4>
                             <p class="text-black-50 mb-0">Breve descrição do pilar MEGA</p>
-                            
                         </div>
-
                     </div>
-                    
-                                                       <div class="col-xl-4 col-lg-5">
+                    <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>Pilar ET</h4>
                             <p class="text-black-50 mb-0">Breve descrição do pilar ET</p>
-                            
                         </div>
-
                     </div>
-                 
-                 
                  </div> 
-                 
-
-                    
-
-                
-                
-                
-                <!-- Project One Row-->
-               
-                <!-- Project Two Row-->
-
             </div>
         </section>
-        <!-- Signup-->
-       
-        <!-- Contact-->
-
         <!-- Footer-->
         <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy;  2021</div></footer>
         <!-- Bootstrap core JS-->
