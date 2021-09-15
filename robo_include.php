f058e942de3b116e151a7841d6ea2c8bfc3a3483
Silva <Gabriel.Silva14@br.nestle.com>
2021-09-15 12:31:49 -0300

    criado modelo bbs e posicionados os inputs

--- robo_include.php
+++ robo_include.php
@@ -2,29 +2,22 @@
 
 
 #robo-pop{
-                position: fixed;
-
-                right:300px;
-
-                bottom: 50px;
-
-                width:80px;
-
-                height: 400px;
-
-                border-radius: 50%;
-
-                opacity: 0.8;
-
-                transition: opacity 0.3s;
-                
-                
-            }
-
-            .chat-btn:hover, .submit:hover, #emoji-btn:hover{
-                opacity: 1;
-            }
-
+    position: fixed;
+    right:0;
+    bottom: 50px;
+    width: 350px;
+    height: 450px;
+    border-radius: 50%;
+    opacity: 0.8;
+    transition: opacity 0.3s;
+}
+.chat-btn:hover, .submit:hover, #emoji-btn:hover{
+    opacity: 1;
+}
+#robo-pop img{
+    width: 350px;
+    height: 450px;
+}
 
 
 
